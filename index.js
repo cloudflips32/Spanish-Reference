82e2bcf2cab0a4cfe37dfeeb0dd63ab2144b0fea
Adam Standish <devadamstandish@gmail.com>
2024-07-06 17:45:49 -0400

    js test

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  return 0;
+document.addEventListener("DOMContentLoaded", function(x=1,y=2) {
+  let sum = x + y; 
+  console.log(sum);
 });
